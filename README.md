ebcb7bdc314f15afe892e28b5cdac54afa656805
Alex Holachek <aholachek@gmail.com>
2019-01-11 14:20:36 -0500

    add a readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-wip
+# React 2019 Interactive Workbook
+
+### React Hooks? Context? Lazy? Memo? Who has time to learn about all of this stuff?
+
+<img src="src/App/About/oldman.jpg" alt="old man yells at cloud">
+
+(Ok, I have the time, I just fear change.)
+
+Just reading the docs without putting your new knowledge into practice isn't a great way to learn, so I've made a few practice tasks that should give you some concrete experience implementing these new(ish) patterns.
+
+## How to get started
+
+Either [fork the CodeSanbox](https://codesandbox.io/s/github/aholachek/react-interactive-workbook) (recommended approach) or fork this repo and run `yarn` and `yarn start`.
+
+Next, click on a task in the menu and follow the instructions! Each task should take between 5 and 25 minutes to complete. For most tasks, you'll see three cards:
+
+1. A set of instructions
+2. A non-working component that you must fix by editing the files in the relevant folder
+3. An example of the component in it's final, working state.
+
+If you get confused, search for the `TODO` comments in the code, or (if you must) take a peek at the `answers` folder in each task.
--- src/App/About/index.js
+++ src/App/About/index.js
@@ -17,20 +17,26 @@ const About = () => {
       <p>(Ok, I have the time, I just fear change.)</p>
 
       <p>
-        Just reading the docs without practicing isn't a great way to retain
-        knowledge, so I've made a few practice tasks that should give you some
-        concrete experience implementing these new(ish) patterns.
+      Just reading the docs without putting your new knowledge into practice isn't a great way to learn, so I've made a few practice tasks that should give you some concrete experience implementing these new(ish) patterns.
       </p>
       <p>
         To get started,{" "}
         <Link to="/context">just click on a task in the menu</Link> and follow
         the instructions! Each task should take between 5 and 25 minutes to
-        complete.
+        complete. For most tasks, you'll see three cards:
       </p>
+      <ol>
+        <li>A set of instructions</li>
+        <li>A non-working component that you must fix by editing the files in the relevant folder</li>
+        <li>An example of the component in it's final, working state.</li>
+      </ol>
       <p>
-        If you get confused, search for the <code>TODO</code> comments in the
-        code.
+      If you get confused, search for the <code>TODO</code> comments in the code, or (if you must) take a peek at the <code>answers</code> folder in each task.
       </p>
+
+
+
+
     </div>
   )
 }
