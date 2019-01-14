dab2749aec5a2e8f90ded9c98aed64d2f5c3ad57
Alex Holachek <aholachek@gmail.com>
2019-01-14 12:57:52 -0500

    default instructions

--- src/App/index.js
+++ src/App/index.js
@@ -141,9 +141,7 @@ function App() {
             ))}
           </ul>
           <main>
-            <Route path="/about" component={About} />
-            <Route path="/" exact render={() => <Redirect to="/context" />} />
-
+            <Route path="/" component={About} />
             {routeConfig.map(({ route, component: Component, title }, i) => (
               <Route
                 key={route}
